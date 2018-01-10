7d5af95e2ca3d7b1c5ed6e830d9dd0614ada0f33
Vinícius Schlee Tessmann <viniciusschleetessmann@gmail.com>
2018-01-10 16:53:13 -0200

    refact list sends

--- app/Http/Controllers/SendController.php
+++ app/Http/Controllers/SendController.php
@@ -7,168 +7,95 @@ use Maatwebsite\Excel\ExcelServiceProvider;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Http\FormRequest;
+use App\Send;
 
 class SendController extends Controller
 {   
-    public function calculate(Request $request) {
-        return view('calculate');
-    }
-
-    public function calculate2(Request $request) {
-        dd($request);
-    }
 
     public function get() {
 
-        $data = DB::select('SELECT * FROM sends');
+        $send = new Send();
+        $data = $send->findAll();
 
         $response = [];
         foreach ($data as $item) {
-
-            $response[$item->type][$item->id] = [
-                'min' => $item->min,
-                'max' => $item->max,
-                'l1' => $item->l1,
-                'l2' => $item->l2,
-                'l3' => $item->l3,
-                'l4' => $item->l4,
-                'e1' => $item->e1,
-                'e2' => $item->e2,
-                'e3' => $item->e3,
-                'e4' => $item->e4,
-                'n1' => $item->n1,
-                'n2' => $item->n2,
-                'n3' => $item->n3,
-                'n4' => $item->n4,
-                'n5' => $item->n5,
-                'n6' => $item->n6,
-                'i1' => $item->i1,
-                'i2' => $item->i2,
-                'i3' => $item->i3,
-                'i4' => $item->i4,
-                'i5' => $item->i5,
-                'i6' => $item->i6,
+            
+            $response[$item['type']][$item['id']] = [
+                'min' => $item['min'],
+                'max' => $item['max'],
+                'l1' => $item['l1'],
+                'l2' => $item['l2'],
+                'l3' => $item['l3'],
+                'l4' => $item['l4'],
+                'e1' => $item['e1'],
+                'e2' => $item['e2'],
+                'e3' => $item['e3'],
+                'e4' => $item['e4'],
+                'n1' => $item['n1'],
+                'n2' => $item['n2'],
+                'n3' => $item['n3'],
+                'n4' => $item['n4'],
+                'n5' => $item['n5'],
+                'n6' => $item['n6'],
+                'i1' => $item['i1'],
+                'i2' => $item['i2'],
+                'i3' => $item['i3'],
+                'i4' => $item['i4'],
+                'i5' => $item['i5'],
+                'i6' => $item['i6'],
             ];
 
         }
 
         return view('values', ['data' => $response]);
-
     }
 
     public function import() {
 
+        //Erase all data before insert new import.
         DB::delete('delete from sends');
-
-        set_time_limit(4000);
-        ini_set('memory_limit', '2048M');
 
         $info = $this->readCsv();
 
         foreach ($info as $item) {
 
-            DB::insert(
-                'insert into sends (
-                    min, 
-                    max, 
-                    type,
-                    l1,
-                    l2,
-                    l3,
-                    l4,
-                    e1,
-                    e2,
-                    e3,
-                    e4,
-                    n1,
-                    n2,
-                    n3,
-                    n4,
-                    n5,
-                    n6,
-                    i1,
-                    i2,
-                    i3,
-                    i4,
-                    i5,
-                    i6
-                ) values (
-                    ?, 
-                    ?, 
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?
-                )', 
-                [
-                    floatval($item['min']), 
-                    floatval($item['max']), 
-                    $item['type'],
-                    (isset($item['L1'])) ? $item['L1'] : null,
-                    (isset($item['L2'])) ? $item['L2'] : null,
-                    (isset($item['L3'])) ? $item['L3'] : null,
-                    (isset($item['L4'])) ? $item['L4'] : null,
-                    (isset($item['E1'])) ? $item['E1'] : null,
-                    (isset($item['E2'])) ? $item['E2'] : null,
-                    (isset($item['E3'])) ? $item['E3'] : null,
-                    (isset($item['E4'])) ? $item['E4'] : null,
-                    (isset($item['N1'])) ? $item['N1'] : null,
-                    (isset($item['N2'])) ? $item['N2'] : null,
-                    (isset($item['N3'])) ? $item['N3'] : null,
-                    (isset($item['N4'])) ? $item['N4'] : null,
-                    (isset($item['N5'])) ? $item['N5'] : null,
-                    (isset($item['N6'])) ? $item['N6'] : null,
-                    (isset($item['I1'])) ? $item['I1'] : null,
-                    (isset($item['I2'])) ? $item['I2'] : null,
-                    (isset($item['I3'])) ? $item['I3'] : null,
-                    (isset($item['I4'])) ? $item['I4'] : null,
-                    (isset($item['I5'])) ? $item['I5'] : null,
-                    (isset($item['I6'])) ? $item['I6'] : null
-                ]
-            );
+            $send = new Send();
+
+            $send->setType($item['type']);
+            $send->setMin($this->converterFloat($item['min']));
+            $send->setMax($this->converterFloat($item['max']));
+
+            $send->setL1((isset($item['L1'])) ? $item['L1'] : null);
+            $send->setL2((isset($item['L2'])) ? $item['L2'] : null);
+            $send->setL3((isset($item['L3'])) ? $item['L3'] : null);
+            $send->setL4((isset($item['L4'])) ? $item['L4'] : null);
+
+            $send->setE1((isset($item['E1'])) ? $item['E1'] : null);
+            $send->setE2((isset($item['E2'])) ? $item['E2'] : null);
+            $send->setE3((isset($item['E3'])) ? $item['E3'] : null);
+            $send->setE4((isset($item['E4'])) ? $item['E4'] : null);
+
+            $send->setN1((isset($item['N1'])) ? $item['N1'] : null);
+            $send->setN2((isset($item['N2'])) ? $item['N2'] : null);
+            $send->setN3((isset($item['N3'])) ? $item['N3'] : null);
+            $send->setN4((isset($item['N4'])) ? $item['N4'] : null);
+            $send->setN5((isset($item['N5'])) ? $item['N5'] : null);
+            $send->setN6((isset($item['N6'])) ? $item['N6'] : null);
+
+            $send->setI1((isset($item['I1'])) ? $item['I1'] : null);
+            $send->setI2((isset($item['I2'])) ? $item['I2'] : null);
+            $send->setI3((isset($item['I3'])) ? $item['I3'] : null);
+            $send->setI4((isset($item['I4'])) ? $item['I4'] : null);
+            $send->setI5((isset($item['I5'])) ? $item['I5'] : null);
+            $send->setI6((isset($item['I6'])) ? $item['I6'] : null);
+
+            $send->save();
+
         }
 
         die;
         
     }
-
-    public function importState() {
-
-        $this->importStates();
-        die;
-
-        $path = url('/') . Storage::get('teste3.csv');
-
-
-        $response = explode("\n", $path);
-
-        for ($x=2; $x<=29; $x++) {
-            $data[] = explode(';', $response[$x]);
-        }
-
-        echo '<pre>';
-        var_dump($data);
-        die;
-    }
-
-
 
     private function readCsv() {
 
@@ -218,40 +145,7 @@ class SendController extends Controller
     }
 
     private function converterFloat($info) {
-
         return floatval(str_replace(',', '.', rtrim($info)));
     }
 
-    // private function readSheet() {
-
-    //     $excel = new \Excel();
-    //     $address = Storage::disk('local')->url('ECONÔMICO-Tabela1.csv');
-
-
-    //     $path = Storage::get('teste.csv');
-
-    //     echo '<pre>';
-    //     var_dump($path);
-    //     die;
-
-
-    //     $fileHandle = fopen($path, "r");
-        
-    //     //Loop through the CSV rows.
-    //     while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
-    //         //Dump out the row for the sake of clarity.
-    //         var_dump($row);
-    //     }
-
-    //     die;
-    //     $response = null;
-
-    //     $excel::load($address, function($reader) {
-    //         $response = $reader->all()->toArray();
-            
-    //         dd($response);
-    //     });
-
-        
-    // }
 }
--- app/Send.php
+++ app/Send.php
@@ -6,7 +6,193 @@ use Illuminate\Database\Eloquent\Model;
 
 class Send extends Model
 {
-    protected $table = 'values';
+    protected $table = 'sends';
 
-    protected $fillable = ['id', 'peso', 'type', 'l1', 'l2', 'l3', 'l4'];
+    public function setMin($min) {
+        $this->attributes['min'] = $min;
+    }
+
+    public function setMax($max) {
+        $this->attributes['max'] = $max;
+    }
+
+    public function setType($type) {
+        $this->attributes['type'] = $type;
+    }
+
+    public function setL1($l1) {
+        $this->attributes['l1'] = $l1;
+    }
+
+    public function setL2($l2) {
+        $this->attributes['l2'] = $l2;
+    }
+
+    public function setL3($l3) {
+        $this->attributes['l3'] = $l3;
+    }
+
+    public function setL4($l4) {
+        $this->attributes['l4'] = $l4;
+    }
+
+    public function setE1($e1) {
+        $this->attributes['e1'] = $e1;
+    } 
+
+    public function setE2($e2) {
+        $this->attributes['e2'] = $e2;
+    }
+
+    public function setE3($e3) {
+        $this->attributes['e3'] = $e3;
+    }
+
+    public function setE4($e4) {
+        $this->attributes['e4'] = $e4;
+    }
+
+    public function setN1($n1) {
+        $this->attributes['n1'] = $n1;
+    } 
+
+    public function setN2($n2) {
+        $this->attributes['n2'] = $n2;
+    }
+
+    public function setN3($n3) {
+        $this->attributes['n3'] = $n3;
+    }
+
+    public function setN4($n4) {
+        $this->attributes['n4'] = $n4;
+    }
+
+    public function setN5($n5) {
+        $this->attributes['n5'] = $n5;
+    }
+
+    public function setN6($n6) {
+        $this->attributes['n6'] = $n6;
+    }
+
+    public function setI1($i1) {
+        $this->attributes['i1'] = $i1;
+    } 
+
+    public function setI2($i2) {
+        $this->attributes['i2'] = $i2;
+    }
+
+    public function setI3($i3) {
+        $this->attributes['i3'] = $i3;
+    }
+
+    public function setI4($i4) {
+        $this->attributes['i4'] = $i4;
+    }
+
+    public function setI5($i5) {
+        $this->attributes['i5'] = $i5;
+    }
+
+    public function setI6($i6) {
+        $this->attributes['i6'] = $i6;
+    }
+
+    public function getMax() {
+        return $this->attributes['max'];
+    }
+
+    public function getMin() {
+        return $this->attributes['min'];
+    }
+
+    public function getType() {
+        return $this->attributes['type'];
+    }
+
+    public function getL1() {
+        return $this->attributes['l1'];
+    }
+
+    public function getL2() {
+        return $this->attributes['l2'];
+    }
+
+    public function getL3() {
+        return $this->attributes['l3'];
+    }
+
+    public function getL4() {
+        return $this->attributes['l4'];
+    }
+
+    public function getE1() {
+        return $this->attributes['e1'];
+    } 
+
+    public function getE2() {
+        return $this->attributes['e2'];
+    }
+
+    public function getE3() {
+        return $this->attributes['e3'];
+    }
+
+    public function getE4() {
+        return $this->attributes['e4'];
+    }
+
+    public function getN1() {
+        return $this->attributes['n1'];
+    } 
+
+    public function getN2() {
+        return $this->attributes['n2'];
+    }
+
+    public function getN3() {
+        return $this->attributes['n3'];
+    }
+
+    public function getN4() {
+        return $this->attributes['n4'];
+    }
+
+    public function getN5() {
+        return $this->attributes['n5'];
+    }
+
+    public function getN6() {
+        return $this->attributes['n6'];
+    }
+
+    public function getI1() {
+        return $this->attributes['i1'];
+    } 
+
+    public function getI2() {
+        return $this->attributes['i2'];
+    }
+
+    public function getI3() {
+        return $this->attributes['i3'];
+    }
+
+    public function getI4() {
+        return $this->attributes['i4'];
+    }
+
+    public function getI5() {
+        return $this->attributes['i5'];
+    }
+
+    public function getI6() {
+        return $this->attributes['i6'];
+    }
+
+    public function findAll() {
+        return $this::all()->toArray();
+    }
 }
